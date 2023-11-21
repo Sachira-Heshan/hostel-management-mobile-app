a49c8ee52fef9de67e6ee606a9a5c17def9dbbb3
Sachira-Heshan <sachiraheshan99@gmail.com>
2023-11-21 10:30:00 +0530

    Settings page done

--- frontend/src/screens/app/admin/Settings.js
+++ frontend/src/screens/app/admin/Settings.js
@@ -1,49 +1,172 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { black, lightGray, primaryBlue, textLightGray, white } from '../../../constants/Colors';
-import { Button } from 'react-native-paper';
+import { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+   black,
+   lightGray,
+   primaryBlue,
+   textDarkGray,
+   textLightGray,
+   white,
+} from "../../../constants/Colors";
+import { Button, Icon, Switch } from "react-native-paper";
 
 const Settings = ({ navigation }) => {
-    return (
-        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ backgroundColor: white, minHeight: '100%' }} showsVerticalScrollIndicator={false} >
-            <View style={styles.container}>
-                <View style={styles.contentContainer}>
-                    <Text style={styles.title}>Account</Text>
-                    <View style={styles.line}></View>
-                    <Text>Hello Settings</Text>
-                    <Button mode='contained' onPress={() => navigation.navigate('AdminChangeProfileDetails')} style={{ width: '100%', margin: 15, padding: 10 }}>Change Profile Details</Button>
-                    <Button mode='contained' onPress={() => navigation.navigate('AdminChangePassword')} style={{ width: '100%', margin: 15, padding: 10 }}>Change Password</Button>
-                </View>
+   const [complainsNotifications, setComplainsNotifications] = useState(false);
+   const [roomManagementNotifications, setRoomManagementNotifications] =
+      useState(false);
+
+   const toggleComplainsNotifications = () => {
+      setComplainsNotifications(!complainsNotifications);
+   };
+
+   const toggleRoomManagementNotifications = () => {
+      setRoomManagementNotifications(!roomManagementNotifications);
+   };
+
+   return (
+      <ScrollView
+         style={{ flex: 1 }}
+         contentContainerStyle={{
+            backgroundColor: white,
+            minHeight: "100%",
+         }}
+         showsVerticalScrollIndicator={false}
+      >
+         <View style={styles.container}>
+            <View style={styles.contentContainer}>
+               <Text style={styles.title}>Account</Text>
+               <View style={styles.line}></View>
+               <View style={styles.sectionContainer}>
+                  <Button
+                     mode="text"
+                     icon={"account-circle"}
+                     onPress={() =>
+                        navigation.navigate("AdminChangeProfileDetails")
+                     }
+                     textColor={textDarkGray}
+                     style={styles.accountButton}
+                     labelStyle={{ fontSize: 17, fontFamily: "fontRegular" }}
+                     contentStyle={{ justifyContent: "flex-start" }}
+                  >
+                     Change Profile Details
+                  </Button>
+                  <Button
+                     mode="text"
+                     icon={"lock"}
+                     onPress={() => navigation.navigate("AdminChangePassword")}
+                     textColor={textDarkGray}
+                     style={styles.accountButton}
+                     labelStyle={{ fontSize: 17, fontFamily: "fontRegular" }}
+                     contentStyle={{ justifyContent: "flex-start" }}
+                  >
+                     Change Password
+                  </Button>
+               </View>
+               <Text style={styles.title}>Notifications</Text>
+               <View style={styles.line}></View>
+               <View style={styles.sectionContainer}>
+                  <View style={styles.notificationContainer}>
+                     <View style={styles.textIconContainer}>
+                        <Icon
+                           source={"comment-plus"}
+                           color={textDarkGray}
+                           size={16}
+                        />
+                        <Text
+                           style={{
+                              fontFamily: "fontRegular",
+                              fontSize: 16,
+                              marginLeft: 10,
+                           }}
+                        >
+                           Complains Notifications
+                        </Text>
+                     </View>
+                     <Switch
+                        value={complainsNotifications}
+                        onValueChange={toggleComplainsNotifications}
+                        color={primaryBlue}
+                        style={{ marginRight: -10 }}
+                     />
+                  </View>
+                  <View style={styles.notificationContainer}>
+                     <View style={styles.textIconContainer}>
+                        <Icon
+                           source={"office-building-cog"}
+                           color={textDarkGray}
+                           size={16}
+                        />
+                        <Text
+                           style={{
+                              fontFamily: "fontRegular",
+                              fontSize: 16,
+                              marginLeft: 10,
+                           }}
+                        >
+                           Room Management Notifications
+                        </Text>
+                     </View>
+                     <Switch
+                        value={roomManagementNotifications}
+                        onValueChange={toggleRoomManagementNotifications}
+                        color={primaryBlue}
+                        style={{ marginRight: -10 }}
+                     />
+                  </View>
+               </View>
             </View>
-        </ScrollView>
-    )
-}
+         </View>
+      </ScrollView>
+   );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: white
-    },
-    contentContainer: {
-        flex: 1,
-        width: '90%',
-        alignItems: 'center',
-    },
-    title: {
-        width: '90%',
-        fontFamily: 'fontBold',
-        fontSize: 18,
-        marginVertical: 10,
-    },
-    line: {
-        backgroundColor: textLightGray,
-        height: 1,
-        width: '100%',
-    },
-})
+   container: {
+      flex: 1,
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: white,
+   },
+   contentContainer: {
+      flex: 1,
+      width: "90%",
+      alignItems: "center",
+   },
+   title: {
+      width: "90%",
+      fontFamily: "fontBold",
+      fontSize: 18,
+      marginVertical: 10,
+   },
+   line: {
+      backgroundColor: textLightGray,
+      height: 1,
+      width: "100%",
+   },
+   sectionContainer: {
+      width: "85%",
+      flexDirection: "column",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+   },
+   accountButton: {
+      marginTop: 10,
+   },
+   notificationContainer: {
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingTop: 5,
+      paddingLeft: 12,
+   },
+   textIconContainer: {
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+   },
+});
 
-export default Settings
+export default Settings;
