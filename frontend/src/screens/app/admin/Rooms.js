48b0e92f8b52d4ef5aea77d066c9482b5381d230
Sachira-Heshan <sachiraheshan99@gmail.com>
2023-11-24 09:30:50 +0530

    Room management home page done

--- frontend/src/screens/app/admin/Rooms.js
+++ frontend/src/screens/app/admin/Rooms.js
@@ -1,44 +1,206 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { black, lightGray, primaryBlue, white } from '../../../constants/Colors';
-import { Button } from 'react-native-paper';
+import { View, Text, Image, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+   black,
+   lightGray,
+   primaryBlue,
+   textLightGray,
+   white,
+} from "../../../constants/Colors";
+import { Button, TouchableRipple, Avatar } from "react-native-paper";
 
 const Rooms = ({ navigation }) => {
-    return (
-        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ backgroundColor: white, minHeight: '100%' }} showsVerticalScrollIndicator={false} >
-            <View style={styles.container}>
-                <View style={styles.contentContainer}>
-                    <Text>Hello Rooms</Text>
-                    <Button mode='contained' onPress={() => navigation.navigate('AdminPendingRoomRequests')} style={{ width: '100%', margin: 15, padding: 10 }}>Pending</Button>
-                    <Button mode='contained' onPress={() => navigation.navigate('AdminAcceptedRoomRequests')} style={{ width: '100%', margin: 15, padding: 10 }}>Accepted</Button>
-                    <Button mode='contained' onPress={() => navigation.navigate('AdminRoomDetails')} style={{ width: '100%', margin: 15, padding: 10 }}>Room Details</Button>
-                    <Button mode='contained' onPress={() => navigation.navigate('AdminVacateRoom')} style={{ width: '100%', margin: 15, padding: 10 }}>Vacate Room</Button>
-                </View>
+   var date = new Date();
+   var hours = date.getHours();
+   var greet = "Greetings!";
+
+   //Greeting based on the time of the day
+   if (hours < 12) {
+      greet = "Good Morning!";
+   } else if (hours < 15) {
+      greet = "Good Afternoon!";
+   } else {
+      greet = "Good Evening!";
+   }
+
+   //Get dummy data from the json file
+   const data = require("../../../data/dummyData.json");
+
+   return (
+      <ScrollView
+         style={{ flex: 1 }}
+         contentContainerStyle={{ backgroundColor: white, minHeight: "100%" }}
+         showsVerticalScrollIndicator={false}
+      >
+         <View style={styles.container}>
+            <View style={styles.contentContainer}>
+               <View style={styles.profileDetails}>
+                  <Avatar.Image
+                     size={75}
+                     source={require("../../../../assets/images/profile_pic.png")}
+                  />
+                  <View style={styles.profileText}>
+                     <Text
+                        style={{
+                           fontFamily: "fontRegular",
+                           fontSize: 16,
+                           color: textLightGray,
+                        }}
+                     >
+                        {greet}
+                     </Text>
+                     <Text
+                        style={{
+                           fontFamily: "fontBold",
+                           fontSize: 16,
+                           marginTop: -5,
+                        }}
+                     >
+                        {data.users[1].fullname}
+                     </Text>
+                  </View>
+               </View>
+               <View style={styles.quickButtons}>
+                  <TouchableRipple
+                     onPress={() =>
+                        navigation.navigate("AdminPendingRoomRequests")
+                     }
+                     style={styles.roomManagementCard}
+                  >
+                     <View
+                        style={{
+                           alignItems: "center",
+                           paddingHorizontal: 15,
+                           paddingVertical: 20,
+                        }}
+                     >
+                        <Image
+                           source={require("../../../../assets/images/pending_room_requests.png")}
+                           style={styles.cardImg}
+                        />
+                        <Text style={styles.cardText}>
+                           Pending Room Requests
+                        </Text>
+                     </View>
+                  </TouchableRipple>
+
+                  <TouchableRipple
+                     onPress={() =>
+                        navigation.navigate("AdminAcceptedRoomRequests")
+                     }
+                     style={styles.roomManagementCard}
+                  >
+                     <View
+                        style={{
+                           alignItems: "center",
+                           paddingHorizontal: 15,
+                           paddingVertical: 20,
+                        }}
+                     >
+                        <Image
+                           source={require("../../../../assets/images/accepted_requests.png")}
+                           style={styles.cardImg}
+                        />
+                        <Text style={styles.cardText}>Accepted Requests</Text>
+                     </View>
+                  </TouchableRipple>
+               </View>
+
+               <View style={styles.quickButtons}>
+                  <TouchableRipple
+                     onPress={() => navigation.navigate("AdminRoomDetails")}
+                     style={styles.roomManagementCard}
+                  >
+                     <View
+                        style={{
+                           alignItems: "center",
+                           paddingHorizontal: 15,
+                           paddingVertical: 20,
+                        }}
+                     >
+                        <Image
+                           source={require("../../../../assets/images/view_room_details.png")}
+                           style={styles.cardImg}
+                        />
+                        <Text style={styles.cardText}>View Room Details</Text>
+                     </View>
+                  </TouchableRipple>
+
+                  <TouchableRipple
+                     onPress={() => navigation.navigate("AdminVacateRoom")}
+                     style={styles.roomManagementCard}
+                  >
+                     <View
+                        style={{
+                           alignItems: "center",
+                           paddingHorizontal: 15,
+                           paddingVertical: 20,
+                        }}
+                     >
+                        <Image
+                           source={require("../../../../assets/images/vacate_room.png")}
+                           style={styles.cardImg}
+                        />
+                        <Text style={styles.cardText}>Vacate Room</Text>
+                     </View>
+                  </TouchableRipple>
+               </View>
             </View>
-        </ScrollView>
-    )
-}
+         </View>
+      </ScrollView>
+   );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: white
-    },
-    contentContainer: {
-        flex: 1,
-        width: '90%',
-        alignItems: 'center',
-    },
-    title: {
-        width: '90%',
-        fontFamily: 'Roboto Regular',
-        fontSize: 16,
-        marginVertical: 10,
-    },
-})
+   container: {
+      flex: 1,
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: white,
+   },
+   contentContainer: {
+      flex: 1,
+      width: "90%",
+      alignItems: "center",
+   },
+   profileDetails: {
+      width: "100%",
+      marginTop: 15,
+      flexDirection: "row",
+      alignItems: "center",
+   },
+   title: {
+      width: "90%",
+      fontFamily: "Roboto Regular",
+      fontSize: 16,
+      marginVertical: 10,
+   },
+   profileText: {
+      marginLeft: 20,
+   },
+   quickButtons: {
+      width: "100%",
+      marginTop: 25,
+      flexDirection: "row",
+      justifyContent: "space-between",
+   },
+   roomManagementCard: {
+      width: "48%",
+      backgroundColor: white,
+      borderRadius: 16,
+      elevation: 5,
+   },
+   cardImg: {
+      width: 60,
+   },
+   cardText: {
+      fontFamily: "fontBold",
+      fontSize: 17,
+      marginTop: 5,
+      textAlign: "center",
+   },
+});
 
-export default Rooms
+export default Rooms;
