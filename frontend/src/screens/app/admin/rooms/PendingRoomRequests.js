cd57cbbe573d089bff329c8f3fffd6f2db63161e
Sachira-Heshan <sachiraheshan99@gmail.com>
2023-11-24 13:23:13 +0530

    Pending requests page done

--- frontend/src/screens/app/admin/rooms/PendingRoomRequests.js
+++ frontend/src/screens/app/admin/rooms/PendingRoomRequests.js
@@ -1,40 +1,138 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { black, lightGray, primaryBlue, white } from '../../../../constants/Colors';
-import { Button } from 'react-native-paper';
+import { View, Text, RefreshControl, FlatList, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+   black,
+   lightGray,
+   primaryBlue,
+   textDarkGray,
+   white,
+} from "../../../../constants/Colors";
+import { Button, List } from "react-native-paper";
+import { useCallback, useState } from "react";
 
 const PendingRoomRequests = ({ navigation }) => {
-    return (
-        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ backgroundColor: white, minHeight: '100%' }} showsVerticalScrollIndicator={false} >
-            <View style={styles.container}>
-                <View style={styles.contentContainer}>
-                    <Text>Hello PendingRoomRequests</Text>
-                </View>
+   const [refreshing, setRefreshing] = useState(false);
+
+   const data = require("../../../../data/dummyData.json");
+
+   const onRefresh = useCallback(() => {
+      setRefreshing(true);
+      setTimeout(() => {
+         setRefreshing(false);
+      }, 1500);
+   }, []);
+
+   return (
+      <View style={{ flex: 1, backgroundColor: white, minHeight: "100%" }}>
+         <View style={styles.container}>
+            <View style={styles.contentContainer}>
+               <View style={styles.listContainer}>
+                  <FlatList
+                     data={data.pending_room_requests}
+                     refreshControl={
+                        <RefreshControl
+                           refreshing={refreshing}
+                           onRefresh={onRefresh}
+                        />
+                     }
+                     renderItem={({ item }) => {
+                        return (
+                           <List.Item
+                              key={item.id}
+                              title={item.name}
+                              description={item.faculty}
+                              left={(color = textDarkGray) => (
+                                 <List.Icon
+                                    color={color}
+                                    icon={"account-circle"}
+                                 />
+                              )}
+                              right={(props) => (
+                                 <List.Icon {...props} icon={"chevron-right"} />
+                              )}
+                              style={{
+                                 paddingLeft: 20,
+                                 backgroundColor: lightGray, //remove
+                                 marginVertical: 8, //remove
+                                 borderRadius: 6, //remove
+                              }}
+                              titleStyle={{ fontFamily: "fontRegular" }}
+                              descriptionStyle={{
+                                 fontFamily: "fontRegular",
+                                 marginTop: -5,
+                              }}
+                              onPress={() => {
+                                 navigation.navigate(
+                                    "AdminAcceptedRoomRequests"
+                                 ); //placeholder navigation
+                              }}
+                           />
+                        );
+                     }}
+                     style={styles.listStyles}
+                     showsVerticalScrollIndicator={false}
+                     ListEmptyComponent={
+                        <View>
+                           <Text
+                              style={{
+                                 fontFamily: "fontBold",
+                                 textAlign: "center",
+                                 marginHorizontal: 15,
+                              }}
+                           >
+                              There are no current pending room requests!
+                           </Text>
+                        </View>
+                     }
+                     ListHeaderComponent={
+                        <View>
+                           <Text
+                              style={{
+                                 fontFamily: "fontBold",
+                                 fontSize: 20,
+                                 textAlign: "center",
+                                 marginVertical: 10,
+                              }}
+                           >
+                              Pending Requests
+                           </Text>
+                        </View>
+                     }
+                  />
+               </View>
             </View>
-        </ScrollView>
-    )
-}
+         </View>
+      </View>
+   );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: white
-    },
-    contentContainer: {
-        flex: 1,
-        width: '90%',
-        alignItems: 'center',
-    },
-    title: {
-        width: '90%',
-        fontFamily: 'Roboto Regular',
-        fontSize: 16,
-        marginVertical: 10,
-    },
-})
+   container: {
+      flex: 1,
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: white,
+   },
+   contentContainer: {
+      flex: 1,
+      width: "90%",
+      alignItems: "center",
+   },
+   title: {
+      width: "90%",
+      fontFamily: "Roboto Regular",
+      fontSize: 16,
+      marginVertical: 10,
+   },
+   listContainer: {
+      flex: 1,
+      width: "100%",
+   },
+   listStyles: {
+      flex: 1,
+   },
+});
 
-export default PendingRoomRequests
+export default PendingRoomRequests;
